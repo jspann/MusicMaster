69d9c5bbb7666329cbd60ae74a9eaa4909edca1c
James Spann <thedeveloper4@gmail.com>
2015-07-21 18:41:23 -0400

    Rewriting interface

--- main.py
+++ main.py
@@ -15,50 +15,23 @@ from multiprocessing import Process
 class WindowFrame(wx.Frame):
 	def __init__(self, parent):
 		wx.Frame.__init__(self, parent)
-		self.SetTitle('Message box')
-		self.panel = wx.Panel(self)
-		self.SetSize((3000, 200))
-
 		#load json
 		with open('style.json') as data_file:    
 			data = json.load(data_file)
+		self.SetTitle('Music Master')
+		self.SetSize((data['window-width'], data['window-height']))
+		self.panel = wx.Panel(self)
 
 
-		self.quote = wx.StaticText(self.panel, label="Your quote:")
-		self.result = wx.StaticText(self.panel, label="")
-		self.result.SetForegroundColour(wx.RED)
-		self.button = wx.Button(self.panel, label="Save")
-		self.lblname = wx.StaticText(self.panel, label="Your name:")
-		self.editname = wx.TextCtrl(self.panel, size=(140, -1), style=wx.TE_PROCESS_ENTER)
-		self.editname.SetBackgroundColour(data["commandbox-color"])
-		#self.editname.SetFocus()
 
-		# Set sizer for the frame, so we can change frame size to match widgets
-		self.windowSizer = wx.BoxSizer()
-		self.windowSizer.Add(self.panel, 1, wx.ALL | wx.EXPAND)        
-
-		# Set sizer for the panel content
-		self.sizer = wx.GridBagSizer(5, 5)
-		self.sizer.Add(self.quote, (0, 0))
-		self.sizer.Add(self.result, (0, 1))
-		self.sizer.Add(self.lblname, (1, 0))
-		self.sizer.Add(self.editname, (1, 1))
-		self.sizer.Add(self.button, (2, 0), (1, 2), flag=wx.EXPAND)
-
-		# Set simple sizer for a nice border
-		self.border = wx.BoxSizer()
-		self.border.Add(self.sizer, 1, wx.ALL | wx.EXPAND, 5)
-
-		# Use the sizers
-		self.panel.SetSizerAndFit(self.border)  
-		self.SetSizerAndFit(self.windowSizer)  
-
-		# Set event handlers
-		self.button.Bind(wx.EVT_BUTTON, self.OnButton)
+		self.editname = wx.TextCtrl(self.panel, size=(240, -1), style=wx.TE_PROCESS_ENTER)
 		self.editname.Bind(wx.EVT_KEY_DOWN,self.parseinfo)
 
-	def OnButton(self, e):
-		self.result.SetLabel(self.editname.GetValue())
+
+
+
+	def OnQuit(self, e):
+		self.Close()
 	
 	def parseinfo(self,e):
 		keycode = e.GetKeyCode()
@@ -68,7 +41,7 @@ class WindowFrame(wx.Frame):
 			else:
 				z = Interpreter()
 				#thread.start_new_thread(z.parse(self.editname.GetValue()))
-				Process(target=z.parse,args=('self.editname.GetValue()',)).start()
+				z.parse(self.editname.GetValue())
 			e.EventObject.Navigate()
 		e.Skip()
 
